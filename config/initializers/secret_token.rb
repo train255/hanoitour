f99487ba280bdb139719eb532ec9220ffcff685f
Duy Thanh <thanhdd.it@gmail.com>
2012-07-19 10:00:34 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hanoitour::Application.config.secret_token = '3bff6bd7615527b8dd385467cf6b17103294585432ea1e3b92cc0e2c582921448c6e4a3d60ed4890238e537c84e970e3fe1df7bd1106424bb6241e428d093965'
